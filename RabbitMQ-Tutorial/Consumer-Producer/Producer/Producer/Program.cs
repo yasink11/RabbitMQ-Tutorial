8b0864321cea80d1a1b848e27f7c7b1ab9211a97
Yasin Karacam <yasink@galpi.com.tr>
2023-06-13 16:15:58 +0300

    RabbitMQ

---
+++ RabbitMQ-Tutorial/Consumer-Producer/Producer/Producer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using System.Threading.Tasks;
+
+
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "letterbox",
+    durable: false,
+    exclusive: false,
+    autoDelete:false,
+    arguments:null
+    );
+
+var random = new Random();
+
+var messageId = 1;
+
+
+while(true)
+{
+    var publishingTime=random.Next(1,4);
+    var message = $"MessageId gönderiliyor : {messageId}";
+    var encodedMessage = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish("", "letterbox", null, encodedMessage);
+    Console.WriteLine($"Published message: {message}");
+    Task.Delay(TimeSpan.FromSeconds(publishingTime)).Wait();
+    messageId++; 
+}
